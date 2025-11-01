83ccba78ac3061ca23e6053c4b53e4f85f233774
mamePi <ainoue2018@gmail.com>
2025-11-01 16:53:21 +0900

    split into client side and server side

--- app/account/createaccount.py
+++ app/account/createaccount.py
@@ -1,10 +1,17 @@
-import account
-print("Enter your account name")
-name = input()
-print("Enter your email")
-mail = input()
-print("Enter your avatar")
-avatar = input()
+import requests
 
-accountlist = []
-accountlist.append(account(name, mail, avatar))
+print("Enter your account name:")
+name = input()
+print("Enter your email:")
+mail = input()
+
+data = {
+    "username": name,
+    "email": mail,
+    "status": "inroom",
+    "avatar": None
+}
+
+response = requests.post("http://127.0.0.1:5000/create_account", json=data)
+
+print(response.json())
